f15200b50312bf3df7d64e5c19e2f5c4de3fd5a5
Mikhail Abramishvili <mg@abramishvili.net>
2021-09-20 23:42:17 +0400

    .

--- resources/js/components/khram/pages/type-7/vertical.vue
+++ resources/js/components/khram/pages/type-7/vertical.vue
@@ -6,7 +6,7 @@
                 <div class="subheader">
                     <h1 class="h1-page">{{ page.title }}</h1>
                 </div>
-                <div class="text" style="background: white; box-shadow: none; border: 0; overflow: hidden; width: auto; height: 88vh; padding: 4vh">
+                <div class="text" style="background: none; box-shadow: none; border: 0; overflow: hidden; width: auto; height: 80vh; padding: 1vh">
                     <iframe :src="'/lib/pdfjs/web/viewer.html?file=' + page.pdf" height="80vh" width="100%" style="border: none;"></iframe>
                 </div>
             </div>
